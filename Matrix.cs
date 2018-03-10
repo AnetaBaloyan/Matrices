85423c7655a7d75dc6d2f72c6f002d739a243e25
Aneta Baloyan <anetabaloyan@Anets-MacBook-Pro.local>
2018-03-10 20:47:55 +0400

    second commit

--- Matrix.cs
+++ Matrix.cs
@@ -14,11 +14,11 @@ namespace Matrices
         /// <summary>
         /// The rows.
         /// </summary>
-        private int rows { get; set; }
+        public int rows { get; }
         /// <summary>
         /// The columns.
         /// </summary>
-        private int columns { get; set; }
+        public int columns { get; }
 
         /// <summary>
         /// Initializes a new instance of the Matrix class.
@@ -53,7 +53,7 @@ namespace Matrices
         /// <returns>The equals.</returns>
         /// <param name="a">The first matrix.</param>
         /// <param name="b">The second matrix.</param>
-        public static void equals(out Matrix a, Matrix b)
+        public static void Assign(out Matrix a, Matrix b)
         {
             a = new Matrix(b.rows, b.columns);
 
@@ -78,9 +78,6 @@ namespace Matrices
 
             if (a.rows == b.rows && a.columns == b.columns)
             {
-                result.columns = a.columns;
-                result.rows = a.rows;
-
                 for (int i = 0; i < a.rows; i++)
                 {
                     for (int j = 0; j < b.columns; j++)
@@ -105,9 +102,6 @@ namespace Matrices
 
             if (a.rows == b.rows && a.columns == b.columns)
             {
-                result.columns = a.columns;
-                result.rows = a.rows;
-
                 for (int i = 0; i < a.rows; i++)
                 {
                     for (int j = 0; j < b.columns; j++)
@@ -133,9 +127,6 @@ namespace Matrices
 
             if (a.columns == b.rows)
             {
-                result.rows = a.rows;
-                result.columns = b.columns;
-
                 for (int i = 0; i < a.rows; i++)
                 {
                     for (int j = 0; j < b.columns; j++)
@@ -151,7 +142,7 @@ namespace Matrices
                     }
                 }
             }
-            else throw new System.ArgumentException("Summed matrices of different sizes.");
+             else throw new System.ArgumentException("Multibplied matrices of incompareable sizes.");
 
             return result;
         }
@@ -198,65 +189,314 @@ namespace Matrices
             return result;
         }
 
-        //public static Matrix Inverse(Matrix a)
-        //{
-        //    Matrix result;
+        /// <summary>
+        /// Determines whether a specified instance of <see cref="Matrices.Matrix"/> is equal to another specified <see cref="Matrices.Matrix"/>.
+        /// </summary>
+        /// <param name="a">The first <see cref="Matrices.Matrix"/> to compare.</param>
+        /// <param name="b">The second <see cref="Matrices.Matrix"/> to compare.</param>
+        /// <returns><c>true</c> if <c>a</c> and <c>b</c> are equal; otherwise, <c>false</c>.</returns>
+        public static bool operator ==(Matrix a, Matrix b)
+        {
+            bool result = true;
+            if (a.rows == b.rows && a.columns == b.columns)
+            {
+                for (int i = 0; i < a.rows; i++)
+                {
+                    for (int j = 0; j < a.columns; j++)
+                    {
+                        if(a.matrix[i, j] != b.matrix[i, j])
+                        {
+                            result = false;
+                        };
+                    }
+                }
+                return result;
+            }
+            else return false;
+        }
 
-        //    Matrix.equals(out result, a);
+        public static Matrix Identity(int n)
+        {
+            Matrix result = new Matrix(n, n);
+            for (int i = 0; i < n; i++)
+            {
+                result.matrix[i, i] = 1;
+            }
+            return result;
+        }
 
-        //    for (int j = 0; j < a.columns; j++)
-        //    {
-        //        for (int i = 0; i < a.rows; i++)
-        //        {
-        //            result = DivideRow(result, i, j);
-        //            Console.WriteLine("divide "+ i + "\n");
-        //            result.print();
+        /// <summary>
+        /// Determines whether a specified instance of <see cref="Matrices.Matrix"/> is not equal to another specified <see cref="Matrices.Matrix"/>.
+        /// </summary>
+        /// <param name="a">The first <see cref="Matrices.Matrix"/> to compare.</param>
+        /// <param name="b">The second <see cref="Matrices.Matrix"/> to compare.</param>
+        /// <returns><c>true</c> if <c>a</c> and <c>b</c> are not equal; otherwise, <c>false</c>.</returns>
+        public static bool operator !=(Matrix a, Matrix b)
+        {
+            return (a == b) == false;
+        }
 
-        //        }
+        public static Matrix operator |(Matrix a, Matrix b)
+        {
+            Matrix result= new Matrix(a.rows, a.columns + b.columns);
+            for (int i = 0; i < result.rows; i++)
+            {
+                for (int j = 0; j < result.columns; j++)
+                {
+                    if (j < a.columns)
+                    {
+                        result.matrix[i, j] = a.matrix[i, j];
+                    } else result.matrix[i, j] = b.matrix[i, (j - a.columns)];
+                }
+            }
+            return result;
+        }
 
-        //        result = SubtractAll(result, j);
-        //        Console.WriteLine("subtract " + j + "\n");
-        //        result.print();
-        //    }
+        /// <summary>
+        /// The Inverse of the specified Matrix.
+        /// </summary>
+        /// <returns>The inverse.</returns>
+        /// <param name="a">The Matrix.</param>
+        public static Matrix Inverse(Matrix a)
+        {
+            if (a.columns != a.rows)
+            {
+                Console.WriteLine("The matrix is NOT invertible!");
+                return new Matrix(0, 0);
+            }
+            else
+            {
+                Matrix result = new Matrix(a.rows, a.columns);
+                Matrix workingMatrix = new Matrix(a.rows, 2 * a.columns);
 
-        //    return result;
-        //}
+                workingMatrix = a | Identity(a.rows);
 
-        //private static Matrix DivideRow(Matrix a, int i, int k)
-        //{
-        //    Matrix result;
+                for (int i = 0; i < a.rows; i++)
+                {
+                    workingMatrix = DivideRow(workingMatrix, i);
+                    //Console.WriteLine("divide " + i + "\n");
+                    //result.print();
 
-        //    Matrix.equals(out result, a);
+                    workingMatrix = SubtractAll(workingMatrix, i);
+                    //Console.WriteLine("subtract " + i + "\n");
+                    //result.print();
+                }
 
-        //    for (int j = 0; j < a.columns; j++)
-        //    {
-        //        if (Math.Abs(result.matrix[i, k]) > 0.00000000000)
-        //        {
-        //            result.matrix[i, j] /= result.matrix[i, k];
-        //        }
-        //    }
+                for (int i = 0; i < a.rows; i++)
+                {
+                    for (int j = 0; j < a.columns; j++)
+                    {
+                        result.matrix[i, j] = workingMatrix.matrix[i, j];
+                    }
+                }
 
-        //    return result;
-        //}
+                if (result != Identity(a.rows))
+                {
+                    Console.WriteLine("The matrix is NOT invertible!");
+                    return new Matrix(0, 0);
+                }
+                else
+                {
+                    for (int i = 0; i < a.rows; i++)
+                    {
+                        for (int j = 0; j < a.columns; j++)
+                        {
+                            result.matrix[i, j] = workingMatrix.matrix[i, a.columns + j];
+                        }
+                    }
+                    return result;
+                }
+            }
+        }
 
-        //private static Matrix SubtractAll(Matrix a, int k)
-        //{
-        //    Matrix result;
+        /// <summary>
+        /// Cancelles the kth row of the given matrix.
+        /// </summary>
+        /// <returns>The matrix with cancelled row.</returns>
+        /// <param name="a">The matrix.</param>
+        /// <param name="k">The index of row to divide.</param>
+        private static Matrix DivideRow(Matrix a, int k)
+        {
+            Matrix result;
 
-        //    Matrix.equals(out result, a);
+            Matrix.Assign(out result, a);
 
-        //    for (int i = 0; i < a.rows; i++)
-        //    {
-        //        if(i != k)
-        //        {
-        //            for (int j = 0; j < result.columns; j++)
-        //            {
-        //                result.matrix[i, j] -= result.matrix[k, j]; 
-        //            }
-        //        }
-        //    }
+            int last = result.rows - 1;
 
-        //    return result;
-        //}
+            while(result.matrix[k, k] == 0 && last != k) 
+            {
+                for (int j = k; j < a.columns; j++)
+                {
+                    double temp = result.matrix[k, j];
+                    result.matrix[k, j] = result.matrix[last, j];
+                    result.matrix[last, j] = temp;
+                }
+                //Console.WriteLine("switch " + k + "th with the " + last + "th" + "\n");
+                //result.print();
+
+                last--;
+            }
+
+            if (result.matrix[k, k] != 0)
+            {
+                double divisor = result.matrix[k, k];
+                for (int j = k; j < a.columns; j++)
+                {
+                    result.matrix[k, j] /= divisor;
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Subtracts the row from all other rows of the given matrix.
+        /// </summary>
+        /// <returns>The the resulting matrix.</returns>
+        /// <param name="a">The matrix.</param>
+        /// <param name="k">The index of the row.</param>
+        private static Matrix SubtractAll(Matrix a, int k)
+        {
+            Matrix result;
+
+            Matrix.Assign(out result, a);
+
+            for (int i = 0; i < a.rows; i++)
+            {
+                if (i != k)
+                {
+                    double argument = result.matrix[i, k];
+                    for (int j = 0; j < result.columns; j++)
+                    {
+                        if (argument != 0)
+                        {
+                            result.matrix[i, j] -= (argument * result.matrix[k, j]);
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Transpose the specified matrix.
+        /// </summary>
+        /// <returns>The transpose.</returns>
+        /// <param name="a">The matrix.</param>
+        public static Matrix Transpose(Matrix a) 
+        {
+            Matrix result = new Matrix(a.columns, a.rows);
+            for (int i = 0; i < a.rows; i++)
+            {
+                for (int j = 0; j < a.columns; j++)
+                {
+                    result.matrix[j, i] = a.matrix[i, j];
+                }
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Determines if the matrix is orthogonal.
+        /// </summary>
+        /// <returns><c>true</c>, if the matrix isorthogonal, <c>false</c> otherwise.</returns>
+        /// <param name="a">The matrix.</param>
+        public static bool IsOrthogonal(Matrix a)
+        {
+            return Inverse(a) == Transpose(a);
+        }
+
+        /// <summary>
+        /// Rotates the specified matrix a, by angle t about the origin, clockwise if d is true and counterclockwise otherwise.
+        /// </summary>
+        /// <returns>The rotated matrix.</returns>
+        /// <param name="a">The matrix.</param>
+        /// <param name="t">The angle.</param>
+        /// <param name="d">If set to <c>true</c> clockwise, and counterclockwise otherwise.</param>
+        public static Matrix Rotation(Matrix a, int t, bool d)
+        {
+            Matrix template = new Matrix(2, 2);
+            Matrix result = new Matrix(2, 2);
+            double[,] container;
+
+            if(d)
+            {
+                container = new double[,] { { Math.Cos(t), Math.Sin(t) }, { Math.Sin(-t), Math.Cos(t) } };
+            } 
+            else 
+            {
+                container = new double[,] { { Math.Cos(t), Math.Sin(-t) }, { Math.Sin(t), Math.Cos(t) } };
+            }
+
+            template.matrix = container;
+
+            result = a * template;
+            return result;
+        }
+
+        /// <summary>
+        /// Scaling the specified matrix a by a factor k.
+        /// </summary>
+        /// <returns>The resulting matrix.</returns>
+        /// <param name="a">The matrix.</param>
+        /// <param name="k">The factor k.</param>
+        public static Matrix Scaling(Matrix a, int k)
+        {
+            Matrix result = new Matrix(2, 2);
+
+            result = a * k * Identity(a.columns);
+            return result;
+        }
+
+        /// <summary>
+        /// I don't know what this is.
+        /// </summary>
+        /// <returns>The translated matrix.</returns>
+        public static Matrix Translation()
+        {
+            Matrix result = new Matrix(0, 0);
+            return result;
+        }
+
+        /// <summary>
+        /// The smallest element of the given matrix.
+        /// </summary>
+        /// <returns>The smallest element.</returns>
+        /// <param name="a">The matrix.</param>
+        public static double Min(Matrix a)
+        {
+            double min = a.matrix[0, 0];
+
+            for (int i = 0; i < a.rows; i++)
+            {
+                for (int j = 0; j < a.columns; j++)
+                {
+                    if (min > a.matrix[i, j]) min = a.matrix[i, j];
+                }
+            }
+
+            return min;
+        }
+
+        /// <summary>
+        /// The biggest element of the given matrix.
+        /// </summary>
+        /// <returns>The biggest element.</returns>
+        /// <param name="a">The matrix.</param>
+        public static double Max(Matrix a)
+        {
+            double max = a.matrix[0, 0];
+
+            for (int i = 0; i < a.rows; i++)
+            {
+                for (int j = 0; j < a.columns; j++)
+                {
+                    if (max < a.matrix[i, j]) max = a.matrix[i, j];
+                }
+            }
+
+            return max;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,47 +7,60 @@ namespace Matrices
     {
         public static void Main(string[] args)
         {
-            Matrix myMatrix = new Matrix(2, 3);
+            Matrix myMatrix = new Matrix(2, 2);
 
-            double[,] a = { { 1, 2, 3 }, { 4, 5, 6 } };
+            double[,] a = { { 2, 2 }, { 1, 3 } };
             myMatrix.matrix = a;
+            Console.WriteLine("Matrix A\n");
+            myMatrix.print();
+            Console.WriteLine("_________________\n");
 
             Matrix myOtherMatrix = new Matrix(3, 3);
 
-            double[,] b = { { 1, 2, -1 }, { 2, 2, 4 }, {1, 3, -3} };
+            double[,] b = { { 1, 2, -1 }, { 2, 2, 4 }, {1, 3, -3 } };
             myOtherMatrix.matrix = b;
-
-            myMatrix += myMatrix;
-
-            myMatrix.print();
-
-
-            Console.WriteLine("_______________\n");
-
-            myMatrix -= myMatrix;
-
-            myMatrix.print();
-
-
-            Console.WriteLine("_______________\n");
-
-            myMatrix *= myOtherMatrix;
-
-            myMatrix.print();
-
-            Console.WriteLine("_______________\n");
-
-            myOtherMatrix *= 5;
-
+            Console.WriteLine("Matrix B\n");
             myOtherMatrix.print();
+            Console.WriteLine("_________________\n");
 
-            //Console.WriteLine("_______________\n");
+            //Console.WriteLine("A + A\n");
+            //(myMatrix + myMatrix).print();
 
-            //Matrix inverseOfTheSecondMatrix = Matrix.Inverse(myOtherMatrix);
 
-            //myOtherMatrix.print();
-            //inverseOfTheSecondMatrix.print();
+            Console.WriteLine("A - A\n");
+            (myMatrix - myMatrix).print();
+            Console.WriteLine("_________________\n");
+
+
+            Console.WriteLine("A * A\n");
+            (myMatrix * myMatrix).print();
+            Console.WriteLine("_________________\n");
+
+            Console.WriteLine("B * 5\n");
+            (myOtherMatrix * 5).print();
+            Console.WriteLine("_________________\n");
+
+            Console.WriteLine("Inverse of A\n");
+            Matrix.Inverse(myMatrix).print();
+            Console.WriteLine("_________________\n");
+
+            Console.WriteLine("Inverse of B\n");
+            Matrix.Inverse(myOtherMatrix).print();
+            Console.WriteLine("_________________\n");
+
+            Console.WriteLine("Transpose of B\n");
+            Matrix.Transpose(myOtherMatrix).print();
+            Console.WriteLine("_________________\n");
+
+            Console.WriteLine("min = " + Matrix.Min(myMatrix));
+            Console.WriteLine("max = " + Matrix.Max(myMatrix));
+            Console.WriteLine("_________________\n");
+
+            Console.WriteLine("min = " + Matrix.Min(myOtherMatrix));
+            Console.WriteLine("max = " + Matrix.Max(myOtherMatrix));
+            Console.WriteLine("_________________\n");
 
         }
     }
 }
+
